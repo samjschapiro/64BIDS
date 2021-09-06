1dd1be7ffe32fa92daab38eb2e0ef4b8960a7dcc
samjschapiro <81483219+samueljschapiro@users.noreply.github.com>
2021-09-06 16:46:02 -0500

    Calculated x-comp, y-comp, magnitude, & direction for vectors

--- PointGroups.java
+++ PointGroups.java
@@ -20,18 +20,42 @@ public class PointGroups {
 
         // System.out.println(secondPoints);
 
-        System.out.println(findGroups(firstPoints, secondPoints));
+        findGroups(firstPoints, secondPoints);
 
     }
     public static List<Integer> findGroups(List<Integer> firstPoints, List<Integer> secondPoints) {
         
         int firstSize = firstPoints.size();
+
         int secondSize = secondPoints.size();
         
         ArrayList<Integer> groups = new ArrayList<Integer>();
         
-        ArrayList<Integer> firstPoints_sorted = new ArrayList<Integer>();
-        ArrayList<Integer> secondPoints_sorted = new ArrayList<Integer>();
+        ArrayList<ArrayList<Double>> input = new ArrayList<ArrayList<Double>>();
+        ArrayList<ArrayList<Double>> output = new ArrayList<ArrayList<Double>>();
+
+        for (int i = 0; i < (firstSize / 2) - 1; i++) {
+            ArrayList<Double> point = new ArrayList<Double>();
+            point.add(0, Double.valueOf(firstPoints.get(i))); /* x component */
+            point.add(1, Double.valueOf(firstPoints.get(i+1))); /* y component */
+            point.add(2, Math.sqrt( Math.pow(firstPoints.get(i), 2) + Math.pow(firstPoints.get(i+1), 2) )); /* magnitude */
+            point.add(3, Math.atan(firstPoints.get(i+1) / Double.valueOf(firstPoints.get(i)))); /* direction */
+            input.add(point);
+            i++;
+        }
+
+        for (int i = 0; i < (secondSize / 2) - 1; i++) {
+            ArrayList<Double> point = new ArrayList<Double>();
+            point.add(0, Double.valueOf(secondPoints.get(i))); /* x component */
+            point.add(1, Double.valueOf(secondPoints.get(i+1))); /* y component */
+            point.add(2, Math.sqrt( Math.pow(secondPoints.get(i), 2) + Math.pow(secondPoints.get(i+1), 2) )); /* magnitude */
+            point.add(3, Math.atan( secondPoints.get(i+1) / Double.valueOf(secondPoints.get(i)))); /* direction */
+            output.add(point);
+            i++;
+        }
+
+        System.out.println(input);
+        System.out.println(output);
 
         /* This gets us .1 % */
         for (int i = 1; i <= firstSize / 2; i++) {
